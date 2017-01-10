df2587fd75fa81907d723c9a7eddfca90b70f6e8
Pascal Mahé <Pascal Mah>
2017-01-10 07:45:11 +0100

    Modifys HKL to add default users
    HibernateKickstartListeners now creates 2 default users.

--- src/main/java/fr/pascalmahe/web/HibernateKickstartListener.java
+++ src/main/java/fr/pascalmahe/web/HibernateKickstartListener.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebListener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import fr.pascalmahe.ex.LoginAlreadyExistsException;
 import fr.pascalmahe.services.UserService;
 
 @WebListener
@@ -39,6 +40,19 @@ public class HibernateKickstartListener implements Serializable, ServletContextL
 		UserService.getValidUser("", "");
 		
 		logger.info("There, Hibernate is up and running!");
+
+		String user1 = System.getenv("USER1").trim();
+		String pwd1 = System.getenv("USER_PWD1").trim();
+		if(user1 != null && pwd1 != null){
+			logger.info("Creating first user...");
+			try {
+				UserService.addUser(user1, pwd1);
+				logger.info("First user created.");
+			} catch (LoginAlreadyExistsException laee){
+				logger.info("First user already existed.");
+			}
+			
+		}
 		
 	}
 	
